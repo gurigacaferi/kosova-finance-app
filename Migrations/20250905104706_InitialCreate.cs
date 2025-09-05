18e41027c20124a9307c2f31e423802707c58309
gurigacaferi <158488473+gurigacaferi@users.noreply.github.com>
2025-09-05 15:10:04 +0200

    Add files via upload

---
+++ Migrations/20250905104706_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ATK_Business_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "business_ledger",
+                schema: "public",
+                columns: table => new
+                {
+                    ledger_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    transaction_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    transaction_type = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    category = table.Column<string>(type: "text", nullable: true),
+                    amount = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_business_ledger", x => x.ledger_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "business_ledger",
+                schema: "public");
+        }
+    }
+}
---
+++ Models/BusinessLedger.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ATK_Business_API.Models
+{
+    [Table("business_ledger", Schema = "public")]
+    public class BusinessLedger
+    {
+        [Key]
+        [Column("ledger_id")]
+        public int LedgerId { get; set; }
+
+        [Column("transaction_date")]
+        public DateTime TransactionDate { get; set; }
+
+        [Column("transaction_type")]
+        public string TransactionType { get; set; }
+
+        [Column("description")]
+        public string? Description { get; set; }
+
+        [Column("category")]
+        public string? Category { get; set; }
+
+        [Column("amount")]
+        public decimal Amount { get; set; }
+    }
+}
